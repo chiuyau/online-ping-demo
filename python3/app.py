1723d0cbbeeab550bd13233abc6576a46af37715
eki <e@e.ki>
2020-10-11 17:29:53 +0800

    Update app.py

--- python3/app.py
+++ python3/app.py
@@ -42,6 +42,6 @@ def ping_message(message):
     thread.start()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='127.0.0.1')
 
 
